73909ed17b9661487b98588788ebc9c652d220f5
Quueen <xrissverbal@gmail.com>
2023-04-04 10:15:37 -0700

    4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: parameter
+ *Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+	char rev = s[0];
+
+	while (s[count] != '\0')
+		count++;
+	for (i = 0 ; 1 < count ; i++)
+	{
+		count--;
+		rev = s[i];
+		s[i] = s[count];
+		s[count] = rev;
+	}
+}
