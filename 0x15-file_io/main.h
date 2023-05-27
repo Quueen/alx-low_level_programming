f1063207b736e01481cd3e32d352ad63797559cc
Quueen <xrissverbal@gmail.com>
2023-05-27 02:23:56 -0700

    correction

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int create_empty_file(const char *filename);
+int main(int argc, char *argv[]);
 
 #endif
